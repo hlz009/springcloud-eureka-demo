2abecb8f3c4390977d8fb64086e85891b3d794bf
小智 <583172396@qq.com>
2018-08-03 18:38:04 +0800

    新增Eureka Server 高可用

---
+++ springCloud-eureka/demoEurekaServer2/src/main/java/com/hu/demoEurekaServer2/DemoEurekaServer2Application.java
@@ -0,0 +1,14 @@
+package com.hu.demoEurekaServer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DemoEurekaServer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEurekaServer2Application.class, args);
+	}
+}
